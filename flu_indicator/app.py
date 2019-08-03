8ad99329754a37b754cef4ba2eb3b117829cca1c
Peter Hardy <peterphardy@gmail.com>
2019-08-03 12:19:54 -0700

    flask app running

--- flu_indicator/app.py
+++ flu_indicator/app.py
@@ -1,107 +1,41 @@
 import os
-
-import pandas as pd
+import io
 import numpy as np
 
-import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine
+import keras
+from keras.preprocessing import image
+from keras.preprocessing.image import img_to_array
+from keras.applications.xception import (
+    Xception, preprocess_input, decode_predictions)
+from keras import backend as K
 
-from flask import Flask, jsonify, render_template
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, request, redirect, url_for, jsonify, render_template
 
 app = Flask(__name__)
 
-
-#################################################
-# Database Setup
-#################################################
-
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/bellybutton.sqlite"
-db = SQLAlchemy(app)
-
-# reflect an existing database into a new model
-Base = automap_base()
-# reflect the tables
-Base.prepare(db.engine, reflect=True)
-
-# Save references to each table
-Samples_Metadata = Base.classes.sample_metadata
-Samples = Base.classes.samples
-
-
-@app.route("/")
+@app.route('/')
 def index():
-    """Return the homepage."""
-    return render_template("index.html")
+   return render_template("index.html")
 
+@app.route("/predictor_map.html")
+def maps():
+    """Render Home Page."""
+    return render_template("predictor_map.html")
 
-@app.route("/agechart")
-def agechart():
-    """Return the homepage."""
-    return render_template("age_chart.html")
+@app.route("/cdc_data.html")
+def cdc():
+    """Render Home Page."""
+    return render_template("cdc_data.html")
 
+@app.route("/google_trends.html")
+def google():
+    """Render Home Page."""
+    return render_template("google_trends.html")
 
-@app.route("/names")
-def names():
-    """Return a list of sample names."""
+@app.route("/contact.html")
+def contact():
+    """Render Home Page."""
+    return render_template("contact.html")
 
-    # Use Pandas to perform the sql query
-    stmt = db.session.query(Samples).statement
-    df = pd.read_sql_query(stmt, db.session.bind)
-
-    # Return a list of the column names (sample names)
-    return jsonify(list(df.columns)[2:])
-
-
-@app.route("/metadata/<sample>")
-def sample_metadata(sample):
-    """Return the MetaData for a given sample."""
-    sel = [
-        Samples_Metadata.sample,
-        Samples_Metadata.ETHNICITY,
-        Samples_Metadata.GENDER,
-        Samples_Metadata.AGE,
-        Samples_Metadata.LOCATION,
-        Samples_Metadata.BBTYPE,
-        Samples_Metadata.WFREQ,
-    ]
-
-    results = db.session.query(*sel).filter(Samples_Metadata.sample == sample).all()
-
-    # Create a dictionary entry for each row of metadata information
-    sample_metadata = {}
-    for result in results:
-        sample_metadata["SAMPLE"] = result[0]
-        sample_metadata["ETHNICITY"] = result[1]
-        sample_metadata["GENDER"] = result[2]
-        sample_metadata["AGE"] = result[3]
-        sample_metadata["LOCATION"] = result[4]
-        sample_metadata["BBTYPE"] = result[5]
-        sample_metadata["WFREQ"] = result[6]
-
-    print(sample_metadata)
-    return jsonify(sample_metadata)
-
-
-@app.route("/samples/<sample>")
-def samples(sample):
-    """Return `otu_ids`, `otu_labels`, and `sample_values`."""
-    stmt = db.session.query(Samples).statement
-    df = pd.read_sql_query(stmt, db.session.bind)
-
-    # Filter the data based on the sample number and
-    # only keep rows with values above 1
-    sample_data = df.loc[df[sample] > 1, ["otu_id", "otu_label", sample]]
-    # Format the data to send as json
-    data = {
-        "otu_ids": sample_data.otu_id.values.tolist(),
-        "sample_values": sample_data[sample].values.tolist(),
-        "otu_labels": sample_data.otu_label.tolist(),
-    }
-    return jsonify(data)
-
-
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+   app.run(debug = True)
